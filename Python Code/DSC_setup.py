60026b1fba109c2498f4e4859c0188b69383e77b
leemjin <101371829+leemjin@users.noreply.github.com>
2022-07-22 10:38:41 -0400

    Setup file for DSC replication

---
+++ Python Code/DSC_setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+import ot
+import cvxpy as cp
+
+# Supplementary Packages
+#import scipy.stats as stats
+#import seaborn as sns
+#import scipy.special as sps
+#import time as t
+
+
+# ## Functions
+
+# In[62]:
+
+
+def baryc_proj(source, target, method):
+    
+    n1 = source.shape[0]
+    n2 = target.shape[0]   
+    p = source.shape[1]
+    a_ones, b_ones = np.ones((n1,)) / n1, np.ones((n2,)) / n2
+    
+    M = ot.dist(source, target)
+    M = M.astype('float64')
+    M /= M.max()
+    
+    if method == 'emd':
+        OTplan = ot.emd(a_ones, b_ones, M, numItermax = 1e7)
+        
+    elif method == 'entropic':
+        OTplan = ot.bregman.sinkhorn_stabilized(a_ones, b_ones, M, reg = 5*1e-3)
+    
+    # initialization
+    OTmap = np.empty((0, p))
+
+    for i in range(n1):
+        
+        # normalization
+        OTplan[i,:] = OTplan[i,:] / sum(OTplan[i,:])
+    
+        # conditional expectation
+        OTmap = np.vstack([OTmap, (target.T @ OTplan[i,:])])
+    
+    OTmap = np.array(OTmap)
+    
+    return(OTmap)
+
+
+# In[63]:
+
+
+def DSCreplication(target, controls, method = 'emd'):
+    
+    n = target.shape[0]
+    d = target.shape[1]
+    J = len(controls)
+    S = np.mean(target)*n*d*J # Stabilizer: to ground the optimization objective
+    
+    
+    # Barycentric Projection
+    G_list = []
+    proj_list = []
+    for i in range(len(controls)):
+        temp = baryc_proj(target, controls[i], method)
+        G_list.append(temp)
+        proj_list.append(temp - target)
+    
+    
+    # Obtain optimal weights
+    mylambda = cp.Variable(J)
+
+    objective = cp.Minimize(
+                    cp.sum(
+                    cp.sum(
+                    cp.sum([a*b for a,b in zip(mylambda, proj_list)])**2))/S
+                    )
+    
+    constraints = [mylambda >= 0, mylambda <= 1, cp.sum(mylambda) == 1]
+
+    prob = cp.Problem(objective, constraints)
+    prob.solve()
+    
+    weights = mylambda.value
+    replication = sum([a*b for a,b in zip(weights, G_list)])
+    
+    
+    return(weights, replication)
+
