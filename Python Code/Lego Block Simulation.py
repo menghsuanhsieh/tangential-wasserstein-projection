98390c77c95f9e28ba5b504a8650168d15121648
leemjin <101371829+leemjin@users.noreply.github.com>
2023-02-23 10:27:48 -0500

    Update Lego Block Simulation.py

--- Python Code/Lego Block Simulation.py
+++ Python Code/Lego Block Simulation.py
@@ -17,86 +17,8 @@ import scipy.stats as stats
 import seaborn as sns
 import scipy.special as sps
 import time as t
+import twp_utils as twp
 
-
-# ## Functions
-
-# In[2]:
-
-
-def baryc_proj(source, target, method):
-    
-    n1 = source.shape[0]
-    n2 = target.shape[0]
-    p = source.shape[1]
-    a_ones, b_ones = np.ones((n1,)) / n1, np.ones((n2,)) / n2
-    
-    M = ot.dist(source, target)
-    M = M.astype('float64')
-    M /= M.max()
-    
-    if method == 'emd':
-        OTplan = ot.emd(a_ones, b_ones, M, numItermax = 1e7)
-        
-    elif method == 'entropic':
-        OTplan = ot.bregman.sinkhorn_stabilized(a_ones, b_ones, M, reg = 5*1e-3)
-    
-    # initialization
-    OTmap = np.empty((0, p))
-
-    for i in range(n1):
-        
-        # normalization
-        OTplan[i,:] = OTplan[i,:] / sum(OTplan[i,:])
-    
-        # conditional expectation
-        OTmap = np.vstack([OTmap, (target.T @ OTplan[i,:])])
-    
-    OTmap = np.array(OTmap).astype('float32')
-    
-    return(OTmap)
-
-
-# In[3]:
-    
-
-def DSCreplication(target, controls, method = 'emd'):
-    
-    n = target.shape[0]
-    d = target.shape[1]
-    J = len(controls)
-    S = np.mean(target)*n*d*J
-    
-    
-    # Barycentric Projection
-    G_list = []
-    proj_list = []
-    for i in range(len(controls)):
-        temp = baryc_proj(target, controls[i], method)
-        G_list.append(temp)
-        proj_list.append(temp - target)
-    
-    
-    # Obtain optimal weights
-    mylambda = cp.Variable(J)
-
-    objective = cp.Minimize(
-                    cp.sum_squares(
-                    cp.sum([a*b for a,b in zip(mylambda, proj_list)], axis = 0))/S
-                    )
-    
-    constraints = [mylambda >= 0, mylambda <= 1, cp.sum(mylambda) == 1]
-
-    prob = cp.Problem(objective, constraints)
-    prob.solve()
-    
-    weights = mylambda.value
-    projection = sum([a*b for a,b in zip(weights, G_list)])
-    
-    
-    return(weights, projection)
-
-# ## LEGO Blocks
 
 # In[4]:
 
@@ -134,7 +56,7 @@ blockCs = imglist[1:]
 
 ts = t.time()
 
-weightsb, replicationb = DSCreplication(blockT, blockCs, 'emd')
+weightsb, replicationb = twp.tan_wass_proj(blockT, blockCs, 'emd')
 
 te = t.time() - ts
 
